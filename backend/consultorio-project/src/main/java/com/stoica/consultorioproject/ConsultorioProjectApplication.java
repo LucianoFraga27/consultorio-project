1b93e9a980a80c622c81c48f0bd4ac2801edb5ac
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-06-17 10:44:25 -0300

    projeto spring

---
+++ backend/consultorio-project/src/main/java/com/stoica/consultorioproject/ConsultorioProjectApplication.java
@@ -0,0 +1,13 @@
+package com.stoica.consultorioproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioProjectApplication.class, args);
+	}
+
+}
